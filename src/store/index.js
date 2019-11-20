dc77e31afd9feb3d2ff429e435aead295f04f38d
Abbe Keultjes <flauwekeul@gmail.com>
2019-11-20 20:51:48 +0100

    add initial state and prettierrc

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,21 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
+
+const between0And100 = amount => Math.max(0, Math.min(100, amount))
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    energy: 100,
+    money: 50,
+    joy: 100,
+  },
+  mutations: {
+    updateStat(state, { stat, amount }) {
+      state[stat] = between0And100(state[stat] + amount)
+    },
+  },
   actions: {},
-  modules: {}
-});
+  modules: {},
+})
