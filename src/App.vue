b318136eb476b00e66f648c5533227a45bb4df92
Abbe Keultjes <flauwekeul@gmail.com>
2019-11-25 22:47:24 +0100

    add basic styling, fonts and Stat component that renders an icon for each stat

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,27 @@
 <template>
-  <div>
-    <div>Dag {{ day }}</div>
-    <div>Energie: {{ energy }} Geld: {{ money }} Geluk: {{ joy }}</div>
+  <main class="main">
+    <div class="stats">
+      <div class="day">
+        <span class="day__label">Dag</span>
+        <span class="day__value">{{ day }}</span>
+      </div>
+      <Stat name="energy" :value="energy" class="stat" />
+      <Stat name="money" :value="money" class="stat" />
+      <Stat name="joy" :value="joy" class="stat" />
+    </div>
     <button type="button" @click="left">{{ leftChoice.name }}</button>
     <p>{{ description }}</p>
     <button type="button" @click="right">{{ rightChoice.name }}</button>
-  </div>
+  </main>
 </template>
 
 <script>
 import { mapState } from 'vuex'
+import Stat from './components/Stat'
 
 export default {
   name: 'app',
+  components: { Stat },
   computed: {
     ...mapState(['day', 'currentCard', 'energy', 'money', 'joy']),
     description() {
@@ -41,4 +50,56 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+:root {
+  --color-primary: rgba(238, 108, 77, 1);
+  --color-secondary: rgb(106, 199, 253);
+
+  --color-light: rgb(225, 254, 255);
+  --color-grey: rgba(61, 90, 128, 1);
+  --color-dark: rgb(32, 38, 48);
+}
+
+body {
+  margin: 0 5vmin;
+  background-color: var(--color-dark);
+  font-family: 'Rubik', sans-serif;
+  font-size: 4vmin;
+  line-height: 1.4;
+  color: var(--color-light);
+}
+
+.main {
+  max-width: 90vmin;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.stats {
+  display: flex;
+  justify-content: space-around;
+  align-items: baseline;
+  padding: 1vmin 4vmin;
+  background-color: rgba(255, 255, 255, 0.08);
+  border-radius: 0 0 1vmin 1vmin;
+}
+
+.stat {
+  flex: 1;
+  height: 10vmin;
+}
+
+.day {
+  width: 25%;
+}
+.day__label {
+  font-size: 3vmin;
+  margin-right: 1vmin;
+}
+.day__value {
+  font-family: 'Lobster Two', cursive;
+  font-size: 13vmin;
+  color: var(--color-secondary);
+  line-height: 1;
+}
+</style>
---
+++ src/components/Stat.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <svg
+      class="icon"
+      xmlns="http://www.w3.org/2000/svg"
+      v-if="name === 'energy'"
+    >
+      <use class="icon__background" xlink:href="#icon-lightning" />
+      <use
+        class="icon__bar"
+        xlink:href="#icon-lightning"
+        :style="{ clipPath: `inset(${value}% 0 0 0)` }"
+      />
+    </svg>
+    <svg
+      class="icon"
+      xmlns="http://www.w3.org/2000/svg"
+      v-if="name === 'money'"
+    >
+      <use class="icon__background" xlink:href="#icon-euro" />
+      <use
+        class="icon__bar"
+        xlink:href="#icon-euro"
+        :style="{ clipPath: `inset(${value}% 0 0 0)` }"
+      />
+    </svg>
+    <svg class="icon" xmlns="http://www.w3.org/2000/svg" v-if="name === 'joy'">
+      <use class="icon__background" xlink:href="#icon-heart" />
+      <use
+        class="icon__bar"
+        xlink:href="#icon-heart"
+        :style="{ clipPath: `inset(${value}% 0 0 0)` }"
+      />
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  props: { name: String, value: Number },
+}
+</script>
+
+<style>
+.icon {
+  width: 100%;
+  height: 100%;
+}
+
+.icon__background {
+  color: var(--color-grey);
+}
+
+.icon__bar {
+  clip-path: inset(0 0 0 0);
+  color: var(--color-primary);
+  transition: clip-path 1s ease-in-out;
+}
+</style>
