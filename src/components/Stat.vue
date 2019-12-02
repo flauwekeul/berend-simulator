40258c61a9a4ef92e3b0958646e68528c87444b4
Abbe Keultjes <flauwekeul@gmail.com>
2019-12-02 21:35:17 +0100

    add labels to stats

--- src/components/Stat.vue
+++ src/components/Stat.vue
@@ -24,6 +24,15 @@
       <use class="icon__background" xlink:href="#icon-heart" />
       <use class="icon__bar" xlink:href="#icon-heart" :style="{ clipPath }" />
     </svg>
+    <div class="label" v-if="name === 'energy'">
+      <span class="label__value">{{ value }}</span> energie
+    </div>
+    <div class="label" v-if="name === 'money'">
+      <span class="label__value">{{ value }}</span> geld
+    </div>
+    <div class="label" v-if="name === 'joy'">
+      <span class="label__value">{{ value }}</span> geluk
+    </div>
   </div>
 </template>
 
@@ -52,5 +61,38 @@ export default {
   clip-path: inset(0 0 0 0);
   color: var(--color-primary);
   transition: clip-path 1s ease-in-out;
+}
+
+.label {
+  opacity: 0;
+  position: relative;
+  width: 75%;
+  margin: 0 auto;
+  transition: 400ms all;
+  background-color: var(--color-grey);
+  color: var(--color-light);
+  padding: 1vmin;
+  text-align: center;
+  border-radius: 1vmin;
+  font-size: 2.5vmin;
+}
+.label::before {
+  content: '';
+  display: block;
+  position: absolute;
+  bottom: 100%;
+  left: calc(50% - 1vmin);
+  width: 0;
+  height: 0;
+  border-left: 1vmin solid transparent;
+  border-right: 1vmin solid transparent;
+  border-bottom: 1vmin solid var(--color-grey);
+}
+.icon:hover + .label {
+  opacity: 1;
+  margin-top: 1vmin;
+}
+.label__value {
+  color: var(--color-primary);
 }
 </style>
